25a4cb78ff9d45c3a6928827906e3a21dbe55de3
Fernando de Magalhães Toledo <114614766+FernandoDeMagalhaes@users.noreply.github.com>
2022-12-02 10:40:31 -0300

    Update Geral
    Mapa, Player e movimentação pelo mapa, colisão e score adicionados

--- principal.py
+++ principal.py
@@ -1,7 +1,7 @@
 import pygame
 import constantes as c
-import sprites
-import os, sys
+import sprites as s
+import os, sys, math
 
 class Game:
     def __init__(self):
@@ -14,6 +14,21 @@ class Game:
         self.is_running = True
         self.font = pygame.font.match_font(c.FONTE)
         self.upload_files()
+        self.level = c.BOARDS
+        self.player = []
+        self.player_x = c.PLAYER_X
+        self.player_y = c.PLAYER_Y
+        self.direction = 0
+        self.counter = 0
+        self.powerup = False
+        self.power_count = 0
+        self.eaten_bees = [False, False, False, False]
+        self.moving = False
+        self.lives = 3
+        self.startup_counter = 0
+        self.score = 0
+        self.direction_command = 0
+        self.turns_allowed = [False, False, False, False]
     
     def new_game(self):
         #Instancia as classes das sprites
@@ -23,23 +38,81 @@ class Game:
     def run(self):
         #loop do jogo
         self.playing = True
+        self.flicker = False
         while self.playing:
             self.clock.tick(c.FPS)
+
+            if self.counter < 19:
+                self.counter += 1
+                if self.counter > 3:
+                    self.flicker = False
+            else:
+                self.counter = 0
+                self.flicker = True
+
+            if self.powerup and self.power_count < 300:
+                self.power_count += 1
+            elif self.powerup and self.power_count >= 300:
+                self.power_count = 0
+                self.powerup = False
+                self.eaten_bees = [False, False, False, False]
+            
+            if self.startup_counter < 90:
+                self.moving = False
+                self.startup_counter += 1
+            else:
+                self.moving = True
+            
+            self.turns_allowed = self.check_position(self.player_x, self.player_y)
+            
+            if self.moving:
+                self.player_x, self.player_y = self.move_player(self.player_x, self.player_y)
+
+            self.score = self.check_collisions(self.score)
             self.events()
+
+            for i in range(4):
+                if self.direction_command == i and self.turns_allowed[i]:
+                    self.direction = i
+        
+            if self.player_x > c.LARGURA:
+                self.player_x = -40
+            elif self.player_x < -40:
+                self.player_x = (c.LARGURA - 3)
+            
             self.update_sprites()
             self.plot_sprites()
     
     def events(self):
         #define eventos do jogo
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 if self.playing:
                     self.playing = False
                 self.is_running = False
             
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.direction_command = 2
+                elif event.key == pygame.K_DOWN:
+                    self.direction_command = 3
+                elif event.key == pygame.K_RIGHT:
+                    self.direction_command = 0
+                elif event.key == pygame.K_LEFT:
+                    self.direction_command = 1
+            
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_p:
                     self.show_pause()
+                if event.key == pygame.K_UP and self.direction_command == 2:
+                    self.direction_command = self.direction
+                elif event.key == pygame.K_DOWN and self.direction_command == 3:
+                    self.direction_command = self.direction
+                elif event.key == pygame.K_RIGHT and self.direction_command == 0:
+                    self.direction_command = self.direction
+                elif event.key == pygame.K_LEFT and self.direction_command == 1:
+                    self.direction_command = self.direction
     
     def update_sprites(self):
         #atualiza os sprites
@@ -49,6 +122,8 @@ class Game:
         #desenha as sprites
         self.screen.fill(c.PRETO) #limpa a tela
         self.all_sprites.draw(self.screen) #desenha as sprites na tela
+        self.draw_bord(self.level) #desenha o nivel
+        self.draw_player() #desenha o player
         pygame.display.flip()
     
     def upload_files(self):
@@ -88,17 +163,19 @@ class Game:
         )        
         
         self.show_text(
-            "Desenvolvido por Fernando de Magalhães, Max e Ester", 
-            19, 
+            "Desenvolvido por Fernando, Max, Ester e Miguel", 
+            16, 
             c.BRANCO, 
             c.LARGURA/2, 
-            570
+            520
         )
         
         pygame.display.flip()
         self.wait_command()
     
     def show_pause(self):
+
+        self.screen.fill(c.PRETO) 
 
         self.show_text(
             "Pause",
@@ -134,6 +211,8 @@ class Game:
     
     def show_game_over(self):
 
+        self.screen.fill(c.PRETO) 
+
         self.show_text(
             "Game Over", 
             40, 
@@ -152,7 +231,151 @@ class Game:
 
         pygame.display.flip()
         self.wait_command()
+    
+    def draw_bord(self, lvl):
+        num1 = c.ALTURA_1
+        num2 = c.LARGURA_1
+        color = c.AZUL
 
+        self.draw_misc()
+
+        for i in range(len(lvl)):
+            for j in range(len(lvl[i])):
+                if lvl[i][j] == 1:
+                    pygame.draw.circle(self.screen, c.BRANCO, (j * num2 + (0.5 * num2), i*num1 + (0.5*num1)), 3)
+                if lvl[i][j] == 2 and not self.flicker:
+                    pygame.draw.circle(self.screen, c.BRANCO, (j * num2 + (0.5 * num2), i*num1 + (0.5*num1)), 6)
+                if lvl[i][j] == 3:
+                    pygame.draw.line(self.screen, color, (j * num2 + (0.5 * num2), i*num1), (j * num2 + (0.5 * num2), i * num1 + num1), 2)
+                if lvl[i][j] == 4:
+                    pygame.draw.line(self.screen, color, (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 2)
+                if lvl[i][j] == 5:
+                    pygame.draw.arc(self.screen, color, [(j*num2 - (num2 * 0.4)), (i*num1 +(0.5*num1)), num2, num1], 0, c.PI/2, 2)
+                if lvl[i][j] == 6:
+                    pygame.draw.arc(self.screen, color, [(j*num2 + (num2 * 0.5)), (i*num1 +(0.5*num1)), num2, num1], c.PI/2, c.PI, 2)
+                if lvl[i][j] == 7:
+                    pygame.draw.arc(self.screen, color, [(j*num2 + (num2 * 0.5)), (i*num1 - (0.4 * num1)), num2, num1], c.PI, 3*c.PI/2, 2)
+                if lvl[i][j] == 8:
+                    pygame.draw.arc(self.screen, color, [(j*num2 - (num2 * 0.4)), (i*num1 - (0.4 * num1)), num2, num1], 3*c.PI/2, 2*c.PI, 2)
+                if lvl[i][j] == 9:
+                    pygame.draw.line(self.screen, c.BRANCO, (j * num2, i * num1 + (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 2)
+
+    def draw_player(self):
+        # 0 = direita, 1 = esquerda, 2 = cima, 3 = baixo
+
+        if self.direction == 0:
+            self.screen.blit(pygame.transform.flip(s.PLAYER_IMAGES[self.counter // 5],True, False), (self.player_x, self.player_y))
+        elif self.direction == 1:
+            self.screen.blit(s.PLAYER_IMAGES[self.counter // 5], (self.player_x, self.player_y))
+        elif self.direction == 2:
+            self.screen.blit(pygame.transform.rotate(s.PLAYER_IMAGES[self.counter // 5], 270), (self.player_x, self.player_y))
+        elif self.direction == 3:
+            self.screen.blit(pygame.transform.rotate(s.PLAYER_IMAGES[self.counter // 5], 90), (self.player_x, self.player_y))
+
+    def check_position(self, playerx, playery):
+
+        turns = [False, False, False, False]
+        centerx = playerx + 15
+        centery = playery + 15
+        num1 = 16
+        num2 = 16
+        num3 = 9
+
+        if centerx // 30 < 15:
+            if self.direction == 0:
+                if self.level[math.floor(centery / num1)][math.floor((centerx - num3) / num2)] < 3:
+                    turns[1] = True
+            if self.direction == 1:
+                if self.level[math.floor(centery / num1)][math.floor((centerx + num3) / num2)] < 3:
+                    turns[0] = True
+            if self.direction == 2:
+                if self.level[math.floor((centery + num3) / num1)][math.floor(centerx / num2)] < 3:
+                    turns[3] = True
+            if self.direction == 3:
+                if self.level[math.floor((centery - num3) / num1)][math.floor(centerx / num2)] < 3:
+                    turns[2] = True
+            
+            if self.direction == 2 or self.direction == 3:
+                if 7 <= centerx % num2 <= 10:
+                    if self.level[math.floor((centery + num3) / num1)][math.floor(centerx / num2)] < 3:
+                        turns[3] = True
+                    if self.level[math.floor((centery - num3) / num1)][math.floor(centerx / num2)] < 3:
+                        turns[2] = True
+
+                if 7 <= centery % num1 <= 10:
+                    if self.level[math.floor(centery / num1)][math.floor((centerx - num2) / num2)] < 3:
+                        turns[1] = True
+                    if self.level[math.floor(centery / num1)][math.floor((centerx + num2) / num2)] < 3:
+                        turns[0] = True
+            
+            if self.direction == 0 or self.direction == 1:
+                if 7 <= centerx % num2 <= 10:
+                    if self.level[math.floor((centery + num1) / num1)][math.floor(centerx / num2)] < 3:
+                        turns[3] = True
+                    if self.level[math.floor((centery - num1) / num1)][math.floor(centerx / num2)] < 3:
+                        turns[2] = True
+
+                if 7 <= centery % num1 <= 10:
+                    if self.level[math.floor(centery / num1)][math.floor((centerx - num3) / num2)] < 3:
+                        turns[1] = True
+                    if self.level[math.floor(centery / num1)][math.floor((centerx + num3) / num2)] < 3:
+                        turns[0] = True
+                
+
+        else:
+            turns[0] = True
+            turns[1] = True
+
+        return turns
+
+    def move_player(self, playerx, playery):
+        # r, l , u, d
+
+        if self.direction == 0 and self.turns_allowed[0]:
+            playerx += c.PLAYER_SPEED
+        elif  self.direction == 1 and self.turns_allowed[1]:
+            playerx -= c.PLAYER_SPEED
+        if self.direction == 2 and self.turns_allowed[2]:
+            playery -= c.PLAYER_SPEED
+        elif  self.direction == 3 and self.turns_allowed[3]:
+            playery += c.PLAYER_SPEED
+        
+        return playerx, playery
+
+    def check_collisions(self, score):
+
+        centerx = self.player_x + 15
+        centery = self.player_y + 15
+
+        if 0 < self.player_x < 464:
+
+            if self.level[math.floor(centery/c.ALTURA_1)][math.floor(centerx/c.LARGURA_1)] == 1:
+                self.level[math.floor(centery/c.ALTURA_1)][math.floor(centerx/c.LARGURA_1)] = 0
+                score += 10
+
+            if self.level[math.floor(centery/c.ALTURA_1)][math.floor(centerx/c.LARGURA_1)] == 2:
+                self.level[math.floor(centery/c.ALTURA_1)][math.floor(centerx/c.LARGURA_1)] = 0
+                score += 50
+                self.powerup = True
+                self.power_count = 0
+                self.eaten_bees = [False, False, False, False]
+        
+        return score
+
+    def draw_misc(self):
+
+        self.show_text(
+            f'Score: {self.score:07}', 
+            16,
+            c.BRANCO,
+            58,
+            528)
+        
+        if self.powerup:
+            pygame.draw.circle(self.screen, c.AZUL, (124, 536), 8)
+        
+        for i in range(self.lives):
+            self.screen.blit(pygame.transform.scale(s.PLAYER_IMAGES[0], (20, 20)), (150 + (i * 30), 526))
 
 g = Game()
 g.show_menu()
--- sprites.py
+++ sprites.py
@@ -1,1 +1,10 @@
+import pygame
+import os
 
+#Euler's Fante
+PLAYER_IMAGES = []
+
+imagesdir = os.path.join(os.getcwd(), "imagens")
+
+for i in range(1, 5):
+    PLAYER_IMAGES.append(pygame.transform.scale(pygame.image.load(os.path.join(imagesdir, f"Euler'sFant_{i}.png")), (30, 30)))
